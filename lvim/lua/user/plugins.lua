8c4e3dc5a74da607a69646a166a65078af72f79b
mdsadabwasim <mwasim@camio.app>
2024-05-16 15:41:05 +0530

    Updated noice configuration

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -119,7 +119,17 @@ lvim.plugins = {
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify"
-    }
+    },
+    config = function()
+      require("noice").setup({
+        presets = {
+          bottom_search = false,   -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      })
+    end
   },
   {
     "rcarriga/nvim-notify",
